4494047512d9c3146d277a0648e664b4bb76562f
Glenfiddish <edwinlee0927@hotmail..com>
2020-08-23 21:00:34 +0800

    add new extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,54 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const caseConversion = (text: string) => {
+	const upperCaseExec = /[A-Z]/;
+	const lowerCaseExec = /[a-z]/;
+
+	const textArray = text.split('');
+	const firstLetter = textArray[0];
+	const isToUpperCase = firstLetter.match(lowerCaseExec);
+	const isToLowerCase = firstLetter.match(upperCaseExec);
+	const isAllLetter = text.match(/^[A-Za-z_]+$/);
+
+	if (!isAllLetter || (!isToLowerCase && !isToUpperCase)) {
+		return text;
+	}
+
+	const newTextArray: any[] = [];
+	textArray.forEach((t, index: number) => {
+		const api = isToLowerCase ? 'toLowerCase' : 'toUpperCase';
+		const currentTextIsUpperCase = t.match(upperCaseExec);
+		// const currentTextIsLowerCase = t.match(lowerCaseExec);
+		let newText = [t[api]()];
+		console.log(newTextArray, t);
+		if (index === 0) {
+			newTextArray.push(...newText);
+			return;
+		}
+
+		const preText = textArray[index - 1];
+
+		if (isToLowerCase && t === '_') {
+			return;
+		}
+
+		if (isToLowerCase && preText === '_') {
+			newTextArray.push(t);
+			return;
+		}
+
+		if (isToUpperCase && currentTextIsUpperCase) {
+			newTextArray.push(...['_', t.toUpperCase()]);
+			return;
+		}
+
+		newTextArray.push(...newText);
+	});
+	console.log(newTextArray);
+	return newTextArray.join('');
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -17,11 +65,50 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from demo!');
+		vscode.window.showInformationMessage('Hello World from demo2!');
 	});
 
+	// 字符翻转
+	let codeReverse = vscode.commands.registerCommand('demo.codeReverse', () => {
+		// editor 这个变量并非一定总是有效的值, 当前激活的编辑器
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			let selections = editor.selections;
+			selections.forEach(i => {
+				const text = document.getText();
+				const newText = text.split('').reverse().join('');
+				editor?.edit((editBuilder: vscode.TextEditorEdit) => {
+					editBuilder.replace(i, newText);
+				});
+			});
+		}
+
+	});
+
+	// 转换大小写
+	let toUpperToLower = vscode.commands.registerCommand('demo.toUpperToLower', () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			let selection = editor.selection;
+			const text = document.getText();
+			console.log(text);
+			const newText = caseConversion(text);
+			editor?.edit((editBuilder: vscode.TextEditorEdit) => {
+				editBuilder.replace(selection, newText);
+			});
+		}
+	});
+
+
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(codeReverse);
+	context.subscriptions.push(toUpperToLower);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
